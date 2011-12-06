6ddd71d10c7a08d11e9386c32de2dec8a9417395
Alessandra Leonhardt <Alessandra@Alessandra-PC.(none)>
2011-12-06 01:29:23 -0200

    images added for new offers

--- scripts_db/populate.bat
+++ scripts_db/populate.bat
@@ -1,6 +1,2 @@
-DELETE FROM fornecedor;
-DELETE FROM consumidor;
 DELETE FROM oferta;
-LOAD DATA LOCAL INFILE 'C:/Users/Alessandra/TubaraoUrbano/scripts_db/popfornecedor.txt' INTO TABLE fornecedor LINES TERMINATED BY '\r\n';
-LOAD DATA LOCAL INFILE 'C:/Users/Alessandra/TubaraoUrbano/scripts_db/popconsumidor.txt' INTO TABLE consumidor LINES TERMINATED BY '\r\n';
 LOAD DATA LOCAL INFILE 'C:/Users/Alessandra/TubaraoUrbano/scripts_db/popoferta.txt' INTO TABLE oferta LINES TERMINATED BY '\r\n';
